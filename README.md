281d3c4e37fc1d0aad9cf9469d46d595c8f211c4
Thomas Sapelza <sapelza.thomas@gmail.com>
2017-11-28 01:14:40 +0100

    Caraval-Website release 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Caraval-Website
-Creating a sleek presentation website of the book Caraval by Stephanie Garber featuring HTML5 and a dynamic design with JavaScript
+Creating a sleek presentation website of the book Caraval by Stephanie Garber featuring HTML5 and a modern, materialised and dynamic design with CSS3 and JavaScript
--- www/static/js/script.js
+++ www/static/js/script.js
@@ -1,7 +1,83 @@
-var target = $('.wrapper');
+var $target = $(".section_wrapper");
 
-
-inView('.section').on('enter', function(el){
-    var color = $(el).attr('data-background-color');
-    target.css('background-color', color );
+inView(".section").on("enter", function(el){
+	var color = $(el).attr("section-background-color");
+	$target.css("background-color", color);
 });
+
+// TODO when FAB enters/exits .section_wrapper viewport, enable/disable FAB
+/*inView($target).on("enter", function(el){
+
+});*/
+
+var slideIndexCharacters = 1;
+var slideIndexReviews = 1;
+showDivs(slideIndexCharacters, "characters");
+showDivs(slideIndexReviews, "reviews");
+
+function plusDivs(n, type)
+{
+	switch(type)
+	{
+		case "characters":
+			showDivs(slideIndexCharacters += n, type);
+			break;
+			
+		case "reviews":
+			showDivs(slideIndexReviews += n, type);
+			break;
+	}
+}
+
+function showDivs(n, type)
+{
+	var x;
+	var slideIndex;
+	
+	switch(type)
+	{
+		case "characters":
+			x = document.getElementsByClassName("characterSlides");
+			slideIndex = slideIndexCharacters;
+			break;
+			
+		case "reviews":
+			x = document.getElementsByClassName("reviewSlides");
+			slideIndex = slideIndexReviews;
+			break;
+	}
+	
+	var indexChanged = false;
+	
+	if (n > x.length)
+	{
+		slideIndex = 1
+		indexChanged = true;
+	}    
+	if (n < 1)
+	{
+		slideIndex = x.length
+		indexChanged = true;
+	}
+	
+	for (var i = 0; i < x.length; i++)
+	{
+		x[i].style.display = "none";
+	}
+	
+	x[slideIndex - 1].style.display = "block";
+	
+	if (indexChanged)
+	{
+		switch (type)
+		{
+			case "characters":
+				slideIndexCharacters = slideIndex;
+				break;
+
+			case "reviews":
+				slideIndexReviews = slideIndex;
+				break;
+		}
+	}	
+}
